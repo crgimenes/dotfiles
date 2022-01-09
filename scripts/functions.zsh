ed7abd7fd1eb22ee0b91b7411e0cc2ddb0c750c4
crgimenes <crg@crg.eti.br>
2022-01-09 10:53:54 -0300

    start GitHub functions

--- scripts/functions.zsh
+++ scripts/functions.zsh
@@ -17,4 +17,13 @@ function urlencode() {
     echo $@|jq -sRr @uri
 }
 
+###############################################################################
+# GitHub
+###############################################################################
+
+# Opens the GitHub page of the current repository.
+function og() {
+    open $(git config remote.origin.url |
+        sed "s/git@\(.*\):\(.*\).git/https:\/\/\1\/\2/")
+}
 
