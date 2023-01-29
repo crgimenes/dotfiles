3eb5c534fab3644ce92e09a2be33730390d66abe
crgimenes <crg@crg.eti.br>
2023-01-29 16:56:06 -0300

    using packer

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -10,37 +10,11 @@ if not status then
 end
 ]] --
 
-local fn = vim.fn -- call Vim functions
+require "plugins"
+
 local g = vim.g -- global variables
+local fn = vim.fn -- call Vim functions
 local call = vim.call -- call Vim functions
-
--- Plugin manager (vim-plug) TODO: use packer.nvim
-local plugged = fn.expand("~/.config/nvim/plugged")
-fn.call("plug#begin", { plugged })
-local Plug = fn['plug#']
-
-Plug('nvim-lualine/lualine.nvim') -- statusline
-Plug('w0rp/ale')
-Plug('neoclide/coc.nvim', { branch = 'release' })
-Plug('rhysd/vim-clang-format')
-Plug('junegunn/fzf')
-Plug('junegunn/fzf.vim')
-Plug('buoto/gotests-vim')
-Plug('vimwiki/vimwiki')
-Plug('Shougo/vimproc.vim', { ['do'] = 'make' })
-Plug('chriskempson/base16-vim')
-Plug('fatih/vim-go', { ['do'] = ':GoInstallBinaries' })
-
--- HTML
-Plug('hail2u/vim-css3-syntax')
-Plug('gorodinskiy/vim-coloresque')
-Plug('tpope/vim-haml')
-Plug('mattn/emmet-vim')
-
--- Javascript
-Plug 'jelera/vim-javascript-syntax'
-
-Plug('github/copilot.vim')
 g.copilot_enabled = 0
 
 -- Rust
@@ -48,7 +22,6 @@ g.copilot_enabled = 0
 -- Plug('rust-lang/rust.vim')
 
 
-call('plug#end')
 
 
 
---
+++ config/nvim/lua/plugins.lua
@@ -0,0 +1,50 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+local status, packer = pcall(require, 'packer')
+if not status then
+    print('Packer not found')
+    return
+end
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    -- My plugins here
+
+    use('nvim-lualine/lualine.nvim') -- statusline
+    use('w0rp/ale')
+    use { 'neoclide/coc.nvim', branch = 'release' }
+    use('rhysd/vim-clang-format')
+    use('junegunn/fzf')
+    use('junegunn/fzf.vim')
+    use('buoto/gotests-vim')
+    use('vimwiki/vimwiki')
+    use('Shougo/vimproc.vim')
+    use('chriskempson/base16-vim')
+    use('fatih/vim-go')
+    use('github/copilot.vim')
+
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
