63adac7d0a04e9cf3b989d1f9ec64abe7c973cd1
crgimenes <crg@crg.eti.br>
2023-02-23 10:53:01 -0300

    update go mod

---
+++ updateGoMod.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+find . -name "go.mod" -type f | while read -r file; do
+    dir=$(dirname "$file")
+    cd "$dir" || exit
+
+    go get -u ./...
+    go mod tidy
+
+    git add .
+    git commit -m "update to go mod"
+    git push
+
+
+    if [ ! -d ".git" ]; then
+        echo "Diretório .git não encontrado em $dir"
+        cd - > /dev/null || exit
+        continue
+    fi
+    git add .
+    git commit -m "update to go mod"
+    git push
+
+    cd - > /dev/null || exit
+done
+
