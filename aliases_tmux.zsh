064d0e0326b3b68432851cb2094ade7a9707ce24
crgimenes <crg@crg.eti.br>
2023-08-05 00:44:46 -0300

    update tmux

--- aliases_tmux.zsh
+++ aliases_tmux.zsh
@@ -1,4 +1,8 @@
 alias tn="tmux next"
 alias td='tmux detach'
 alias ntt='tmux new-session -A -s default'
+alias ltc='tmux list-client'
+alias lts='tmux list-sessions'
+alias ltw='tmux list-windows'
+alias ltp='tmux list-panes'
 
