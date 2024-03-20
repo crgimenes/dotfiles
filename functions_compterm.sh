f9f3e53adbd2d221742da22379c9f332ed9f30af
crgimenes <crg@crg.eti.br>
2024-03-20 08:38:18 -0300

    improve compterm detection

--- functions_compterm.sh
+++ functions_compterm.sh
@@ -1,19 +1,4 @@
 function is_parent_compterm {
-    local pid=$$
-    local parent_pid
-
-    while [ "$pid" -ne 1 ]; do
-        parent_pid=$(ps -p $pid -o ppid=)
-        parent_pid=${parent_pid//[[:blank:]]/}  # Remove espaços
-
-        # Verifica se o nome do comando do processo pai é 'compterm'
-        if ps -p $parent_pid -o comm= | grep -q "^compterm$"; then
-            return 0  # Retorna true se encontrar 'compterm'
-        fi
-
-        pid=$parent_pid
-    done
-
-    return 1  # Retorna false se 'compterm' não for encontrado
+    [[ -v COMPTERM ]] || return 1
 }
 
