b6f65718e52a3ccfecad8d9aa57175573fe9d584
crgimenes <crg@crg.eti.br>
2024-03-23 06:18:50 -0300

    small bash improvements

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
-if [[ $# -eq 1 ]]; then
-    selected=$1
-else
+selected=$1
+[[ -z "$selected" ]] && \
     selected=$(find ~/Work ~/Projects ~/Documents ~ -mindepth 1 -maxdepth 1 -type d | fzf)
-fi
 
-if [[ -z "$selected" ]]; then
+[[ -z "$selected" ]] && \
     exit 0
-fi
 
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
-if [[ -z "$TMUX" ]] && [[ -z "$tmux_running" ]]; then
-    tmux new-session -s "$selected_name" -c "$selected"
+[[ -z "$TMUX" ]] && [[ -z "$tmux_running" ]] && \
+    tmux new-session -s "$selected_name" -c "$selected" && \
     exit 0
-fi
 
 if ! tmux has-session -t="$selected_name" 2> /dev/null; then
     tmux new-session -ds "$selected_name" -c "$selected"
