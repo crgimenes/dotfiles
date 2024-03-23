f2120b245507ce21afdfea67af9eb8f45b25d4d2
crgimenes <crg@crg.eti.br>
2024-03-23 07:05:44 -0300

    cleam some bash commands

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-selected=$1
+[[ -z "$TMUX" ]] && \
+    echo "This script must be run from within tmux" && \
+    exit 1
+
 [[ -z "$selected" ]] && \
-    selected=$(find ~/Work ~/Projects ~/Documents ~ -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/Work ~/Projects ~/Documents ~ -mindepth 1 -maxdepth 1 -type d | fzf) || \
+    selected=$1
 
 [[ -z "$selected" ]] && \
     exit 0
 
 selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
-
-[[ -z "$TMUX" ]] && [[ -z "$tmux_running" ]] && \
-    tmux new-session -s "$selected_name" -c "$selected" && \
-    exit 0
 
 if ! tmux has-session -t="$selected_name" 2> /dev/null; then
     tmux new-session -ds "$selected_name" -c "$selected"
