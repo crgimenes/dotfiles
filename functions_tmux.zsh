ba1e085befa11afd07e3eb7abc3327da2a095bd1
crgimenes <crg@crg.eti.br>
2024-05-12 21:16:13 -0300

    update tmux functions

--- functions_tmux.zsh
+++ functions_tmux.zsh
@@ -8,4 +8,21 @@ function msg() {
     done
 }
 
+function t() {
+    SESSION_NAME="default"
+    if [ -n "$1" ]; then
+        SESSION_NAME=$1
+    fi
+
+    if [ -z "$TMUX" ]; then
+        tmux new -A -s "${SESSION_NAME}"
+        return
+    fi
+    
+    if ! tmux has-session -t="${SESSION_NAME}" 2> /dev/null; then
+        tmux new-session -ds "${SESSION_NAME}"
+    fi
+
+    tmux switch-client -t "${SESSION_NAME}"
+}
 
