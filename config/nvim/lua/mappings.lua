24c18a274837d45de15281e7ca417d538b9150bb
crgimenes <crg@crg.eti.br>
2024-05-30 07:15:44 -0300

    experimental config

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -95,6 +95,9 @@ keyset('n', '<cr>', '<Plug>(coc-definition)', { silent = true, noremap = true })
 -- back to previous location with backspace or <C-o>
 keyset('n', '<backspace>', '<C-o>', { silent = true, noremap = true })
 
+-- CoC prevents <cr> from working in insert mode
+keyset('i', '<cr>', 'coc#pum#visible() ? coc#pum#confirm() : "<cr>"', { expr = true })
+
 
 -- cnoremap <expr><Up> pumvisible() ? "\<Left>" : "\<Up>"
 -- cnoremap <expr><Down> pumvisible() ? "\<Right>" : "\<Down>"
