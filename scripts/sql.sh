af8e8525254b7a290f7d554b737c0748aae14edf
crgimenes <crg@crg.eti.br>
2024-06-11 09:21:34 -0300

    draft sql command

--- scripts/sql.sh
+++ scripts/sql.sh
@@ -2,28 +2,32 @@
 
 EDITOR=${EDITOR:-vi}
 
-preview_sql() {
-  case "$2" in
-    sqlite)
-      sqlite3 "$1" ".schema" 2>&1
-      ;;
-    psql)
-      PGPASSWORD=${PGPASSWORD:-postgres} psql -U postgres -h localhost -f "$1" 2>&1
-      ;;
-    *)
-      bat --color=always "$1"
-      ;;
-  esac
+function runcmd() {
+  local file=$1
+  local db=$2
+  echo "Running $db $file"
+  exit 0
 }
+
+# Define cores para o header
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Monta o header com cores
+HEADER=$(printf "${RED}ˆs${NC} sqlite, ${GREEN}^p${NC} psql, ${YELLOW}return${NC} edit")
+
 
 IFS=: read -ra selected < <(
   rg --color=always --line-number --no-heading --smart-case --glob '*.sql' "${*:-}" |
     fzf --ansi \
         --color "hl:-1:underline,hl+:-1:underline:reverse" \
         --delimiter : \
-        --preview 'preview_sql {1} sqlite' \
-        --bind 'ctrl-s:execute-silent(preview_sql {1} sqlite)' \
-        --bind 'ctrl-p:execute-silent(preview_sql {1} psql)' \
+        --preview 'bat --color=always {1} --highlight-line {2}' \
+        --header "$HEADER" \
+        --bind 'ctrl-s:execute(runcmd {1} sqlite)' \
+        --bind 'ctrl-p:execute(runcmd {1} psql)' \
         --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
 )
 
