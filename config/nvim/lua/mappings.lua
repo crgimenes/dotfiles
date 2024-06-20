b2bdb03ea36330f2900e0ef399aa3ea3c01e9112
crgimenes <crg@crg.eti.br>
2024-06-20 09:49:21 -0300

    improve keybinds and add go to implementation

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -91,13 +91,17 @@ keyset('i', '<cr>', 'coc#pum#visible() ? coc#pum#confirm() : "<cr>"', { expr = t
 -- CoC jump to definition
 keyset('n', '<C-]>', '<Plug>(coc-definition)', { silent = true, noremap = true })
 -- keyset('n', 'gd', '<Plug>(coc-definition)', { silent = true, noremap = true })
-keyset('n', '<cr>', '<Plug>(coc-definition)', { silent = true, noremap = true })
+-- keyset('n', '<cr>', '<Plug>(coc-definition)', { silent = true, noremap = true })
+keyset('n', '<A-CR>', '<Plug>(coc-definition)', { silent = true, noremap = true })
 -- back to previous location with backspace or <C-o>
-keyset('n', '<backspace>', '<C-o>', { silent = true, noremap = true })
+-- keyset('n', '<backspace>', '<C-o>', { silent = true, noremap = true })
+keyset('n', '<A-BS>', '<C-o>', { silent = true, noremap = true })
+
+-- go to implementation
+keyset('n', '<leader>gi', '<Plug>(coc-implementation)', { silent = true, noremap = true })
 
 -- CoC prevents <cr> from working in insert mode
-keyset('i', '<cr>', 'coc#pum#visible() ? coc#pum#confirm() : "<cr>"', { expr = true })
-
+-- keyset('i', '<cr>', 'coc#pum#visible() ? coc#pum#confirm() : "<cr>"', { expr = true })
 
 -- cnoremap <expr><Up> pumvisible() ? "\<Left>" : "\<Up>"
 -- cnoremap <expr><Down> pumvisible() ? "\<Right>" : "\<Down>"
