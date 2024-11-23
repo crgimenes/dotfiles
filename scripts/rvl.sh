e5f4d5167edf307e37c86a282ce3c797be4ad84f
crgimenes <crg@crg.eti.br>
2024-11-23 12:50:25 -0300

    format scripts

--- scripts/rvl.sh
+++ scripts/rvl.sh
@@ -3,23 +3,22 @@
 INITIAL_QUERY="${*:-}"
 
 MARKS_FILES=("$HOME/marks.txt")
-if [ -f "./marks.txt" ]; then
+[ -f "./marks.txt" ] && \
     MARKS_FILES+=("./marks.txt")
-fi
 
 IFS='+' read -r file_path line_number <<< "$(
     cat "${MARKS_FILES[@]}" | \
     fzf --ansi \
-        --query "$INITIAL_QUERY" \
         --delimiter '+' \
         --preview 'bat --color=always {1} --highlight-line {2}' \
-        --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
+        --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+        --query "$INITIAL_QUERY"
 )"
 
-if [ -n "$file_path" ]; then
+[ -n "$file_path" ] && {
     file_path=$(echo "$file_path" | xargs)
     line_number=$(echo "$line_number" | xargs)
     "$EDITOR" "$file_path" "+$line_number"
     echo "$file_path" "+$line_number"
-fi
+}
 
